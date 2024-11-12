a6fcd4a46d5ff0d9cd8b9019fde28ed971c84b91
Mohamed Ben Massouda <benmassoudamohamed1@gmail.com>
2024-11-12 15:28:19 +0100

    Utility functions

---
+++ internal/utils/utils.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"errors"
+	"os"
+	"reminder-server/internal/models"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func GenerateToken(user models.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	if jwtSecret == "" {
+		return "", errors.New("JWT_SECRET is not set")
+	}
+
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
