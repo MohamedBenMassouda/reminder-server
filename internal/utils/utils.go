3c3091307be4aa78071d76483b57b673ac069ef5
MohamedBenMassouda <benmassoudamohamed1@gmail.com>
2025-12-01 10:28:59 +0100

    Check priority before creating reminder

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,10 +4,14 @@ import (
 	"errors"
 	"os"
 	"reminder-server/internal/models"
+	"slices"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
+
+var validPrioties = []string{models.PriorityLow, models.PriorityMedium, models.PriorityHigh}
+var valiudStatuses = []string{models.StatusPending, models.StatusCompleted, models.StatusOverdue}
 
 func GenerateToken(user models.User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -32,4 +36,12 @@ func GenerateToken(user models.User) (string, error) {
 
 func GetCurrentTime() time.Time {
 	return time.Now()
+}
+
+func IsValidPriority(priority string) bool {
+	return slices.Contains(validPrioties, priority)
+}
+
+func IsValidStatus(status string) bool {
+	return slices.Contains(valiudStatuses, status)
 }
